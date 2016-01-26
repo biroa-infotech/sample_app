2216b419ae94b97cf7c19150f2c4ded7b13ba921
„Andrej <biroa@infotech.sk>
2016-01-26 14:34:05 +0100

    first commit

--- app/src/main/java/sk/infotech/samplestiapp/ui/fragments/MoviesFragment.java
+++ app/src/main/java/sk/infotech/samplestiapp/ui/fragments/MoviesFragment.java
@@ -5,11 +5,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.util.Pair;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import sk.infotech.samplestiapp.R;
 import sk.infotech.samplestiapp.entities.Movie;
@@ -18,6 +24,7 @@ import sk.infotech.samplestiapp.movies.MovieListPresenter;
 import sk.infotech.samplestiapp.movies.MovieRecyclerAdapter;
 import sk.infotech.samplestiapp.movies.OnItemClickListener;
 import sk.infotech.samplestiapp.ui.activities.MovieDetailActivity;
+import sk.infotech.samplestiapp.ui.activities.ScrollingActivity;
 
 
 /**
@@ -66,12 +73,23 @@ public class MoviesFragment extends Fragment implements IMoviesView {
     }
 
     private void makeTransition(View rowView, Movie movie) {
-        Intent intent = new Intent(getActivity(), MovieDetailActivity.class);
-        intent.putExtra(MovieDetailActivity.EXTRA_MOVIE, movie);
+        Intent intent = new Intent(getActivity(), ScrollingActivity.class);
+        intent.putExtra(ScrollingActivity.EXTRA_MOVIE, movie);
 
-        View image = rowView.findViewById(R.id.movie_image);
+        ImageView movieImage = (ImageView) rowView.findViewById(R.id.movie_image);
+        LinearLayout movieNameHolder = (LinearLayout) rowView.findViewById(R.id.movie_name_holder);
+
+        View statusBar = getActivity().findViewById(android.R.id.statusBarBackground);
+        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
+
+        Pair<View, String> imagePair = Pair.create((View) movieImage, "image");
+        //Pair<View, String> holderPair = Pair.create((View) movieNameHolder, "nameHolder");
+       // Pair<View, String> statusPair = Pair.create(statusBar, Window.STATUS_BAR_BACKGROUND_TRANSITION_NAME);
+       // Pair<View, String> toolbarPair = Pair.create((View)toolbar, "toolbar");
+
         ActivityOptionsCompat options = ActivityOptionsCompat.
-                makeSceneTransitionAnimation(getActivity(), image, "tImage");
+                makeSceneTransitionAnimation(getActivity(), imagePair);
+
 
         getActivity().startActivity(intent, options.toBundle());
     }
